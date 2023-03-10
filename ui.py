e3c70d1bddefa7c0d56414fbeff72cc8f8c414ca
dima999572 <102175283+dima999572@users.noreply.github.com>
2023-03-10 12:03:27 +0100

    Update ui.py

--- ui.py
+++ ui.py
@@ -21,11 +21,11 @@ class QuizzInterface:
         self.question_text = self.canvas.create_text(150, 115, width=280, text="Hi", font=("Arial", 20, "italic"))
         self.canvas.grid(row=1, column=0, columnspan=2, pady=20, padx=20)
 
-        true_img = PhotoImage(file="I00_Days_of_Code/Quizzler_App/images/true.png")
+        true_img = PhotoImage(file="images/true.png")
         self.true_button = Button(image=true_img, highlightthickness=0, command=self.true_pressed)
         self.true_button.grid(row=2, column=0, padx=20, pady=20)
 
-        false_img = PhotoImage(file="I00_Days_of_Code/Quizzler_App/images/false.png")
+        false_img = PhotoImage(file="images/false.png")
         self.false_button = Button(image=false_img, highlightthickness=0, command=self.false_pressed)
         self.false_button.grid(row=2, column=1, padx=20, pady=20)
 
